929f51d1c5bcc4113e400d570f3f53fe87045889
huuphuoc.bui <huuphuoc.bui@nhn.com>
2020-10-20 12:56:49 +0700

    Add input text to query repositories

--- app/src/main/java/com/example/presentation/feature/main/MainFragment.kt
+++ app/src/main/java/com/example/presentation/feature/main/MainFragment.kt
@@ -1,12 +1,15 @@
 package com.example.presentation.feature.main
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.common.BaseFragment
+import com.example.common.extension.dismissKeyboard
 import com.example.presentation.databinding.FragmentMainBinding
 import com.example.presentation.model.RepoItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -31,9 +34,30 @@ class MainFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerRepoItems.adapter = MainListAdapter()
+        initViews()
+    }
 
-        viewModel.searchRepos("android")
+    private fun initViews() {
+        binding.recyclerRepoItems.adapter = MainListAdapter()
+        binding.editQuery.setOnEditorActionListener { view: View, actionId: Int, _: KeyEvent? ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                search(view)
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+        binding.editQuery.setOnKeyListener { view: View, keyCode: Int, event: KeyEvent ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                search(view)
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun search(view: View) {
+        activity?.dismissKeyboard(view.windowToken)
+        viewModel.searchRepos()
     }
 
     object Binding {
--- app/src/main/java/com/example/presentation/feature/main/MainViewModel.kt
+++ app/src/main/java/com/example/presentation/feature/main/MainViewModel.kt
@@ -1,9 +1,8 @@
 package com.example.presentation.feature.main
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.domain.usecase.SearchReposUseCase
+import com.example.lib.extension.orFalse
 import com.example.presentation.mapper.RepoItemMapper
 import com.example.presentation.model.RepoItem
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +10,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
+import java.util.*
 
 class MainViewModel(
     private val searchReposUseCase: SearchReposUseCase,
@@ -20,21 +20,33 @@ class MainViewModel(
     private var searchJob: Job? = null
 
     val repoItem = MutableLiveData<List<RepoItem>>(listOf())
+    val isLoading = MutableLiveData<Boolean>(false)
+    val query = MutableLiveData<String>("")
 
-    fun searchRepos(query: String) {
-        searchJob?.cancel()
-        searchJob = viewModelScope.launch {
-            val result = withContext(Dispatchers.IO) {
-                searchReposUseCase.execute(SearchReposUseCase.Params(query))
-            }
-            result.subscribe(
-                success = { repos ->
-                    repoItem.value = repoItemMapper.mapList(repos)
-                },
-                error = { exception ->
-                    Timber.e(exception)
+    val isNoResults: LiveData<Boolean> = Transformations.map(isLoading) { isLoading ->
+        repoItem.value.isNullOrEmpty() && !query.value.isNullOrEmpty() && !isLoading
+    }
+
+    fun searchRepos() {
+        val query = query.value.orEmpty().toLowerCase(Locale.getDefault()).trim()
+        if (query.isNotEmpty()) {
+            searchJob?.cancel()
+            searchJob = viewModelScope.launch {
+                isLoading.value = true
+                val result = withContext(Dispatchers.IO) {
+                    searchReposUseCase.execute(SearchReposUseCase.Params(query))
                 }
-            )
+                result.subscribe(
+                    success = { repos ->
+                        repoItem.value = repoItemMapper.mapList(repos)
+                        isLoading.value = false
+                    },
+                    error = { exception ->
+                        Timber.e(exception)
+                        isLoading.value = false
+                    }
+                )
+            }
         }
     }
 }
