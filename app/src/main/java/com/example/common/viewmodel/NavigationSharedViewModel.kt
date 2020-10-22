4fe86f9ce6e66ec2c2dc66d8bedbb14056f77122
huuphuoc.bui <huuphuoc.bui@nhn.com>
2020-10-22 15:53:11 +0700

    Provide custom back navigation

--- app/src/main/java/com/example/common/viewmodel/NavigationSharedViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.common.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.common.livedata.SingleLiveData
-
-class NavigationSharedViewModel : ViewModel() {
-
-    val backNavigation = SingleLiveData<Unit>()
-}
