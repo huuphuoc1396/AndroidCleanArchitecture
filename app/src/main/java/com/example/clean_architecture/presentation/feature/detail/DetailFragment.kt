f973b9eb2b6ca1a4debe029ffb629f2fbda0ed4e
huuphuoc1396 <huuphuoc1396@gmail.com>
2022-05-29 16:40:40 +0700

    Migrate Detail Screen to Compose at DetailFragment

--- app/src/main/java/com/example/clean_architecture/presentation/feature/detail/DetailFragment.kt
+++ app/src/main/java/com/example/clean_architecture/presentation/feature/detail/DetailFragment.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.clean_architecture.R
 import com.example.clean_architecture.core.platform.BaseFragment
-import com.example.clean_architecture.core.platform.BaseViewModel
 import com.example.clean_architecture.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,28 +32,54 @@ class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>() {
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentDetailBinding {
-        return FragmentDetailBinding.inflate(inflater, container, false)
+        return FragmentDetailBinding.inflate(inflater, container, false).apply {
+            composeView.setContent {
+                DetailScreen()
+            }
+        }
     }
 
-    override fun onBindVariable() {
-        super.onBindVariable()
-        viewDataBinding.viewModel = viewModel
+    @Composable
+    fun DetailScreen() {
+        val title by viewModel.repoName.observeAsState("")
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = title) },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            findNavController().navigateUp()
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_baseline_arrow_back),
+                                contentDescription = "",
+                            )
+                        }
+                    }
+                )
+            },
+        ) {
+            DetailBody()
+        }
+    }
+
+    @Composable
+    fun DetailBody() {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "Content Body")
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
+        initViewModel()
     }
 
-    private fun initViews() {
+    private fun initViewModel() {
         viewModel.init(
             repoName = args.repoName,
             ownerLogin = args.ownerLogin
         )
-
-        viewDataBinding.toolBar.setNavigationOnClickListener {
-            findNavController().navigateUp()
-        }
     }
 
 }
